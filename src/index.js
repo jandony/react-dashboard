25738b7ee91e0dbb0a2832ceea990e1c8ffc4328
Jeff Andony <jeff.andony@gmail.com>
2024-05-09 12:28:38 -0700

    setting up Redux information & chrome extension

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 const App = lazy(() => import('./App'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Suspense>
-        <App />
-    </Suspense>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Provider store={store} >
+            <Suspense>
+                <App />
+            </Suspense>
+        </Provider>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
