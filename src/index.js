3163c7f1e4760b981ef114d7a2198dc5eb153a92
Jeff Andony <jeff.andony@gmail.com>
2024-05-09 12:50:49 -0700

    adding more redux settings for state output

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import { Toaster } from 'react-hot-toast';
 const App = lazy(() => import('./App'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,6 +14,15 @@ root.render(
         <Provider store={store} >
             <Suspense>
                 <App />
+                <Toaster 
+                    toastOPtions={{
+                        position: 'top-right',
+                        style: {
+                            background: '#283046',
+                            color: 'white'
+                        }
+                    }}
+                />
             </Suspense>
         </Provider>
     </BrowserRouter>
