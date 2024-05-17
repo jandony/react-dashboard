5f6702b909ccc952c631499148db030e12954e16
Jeff Andony <jeff.andony@gmail.com>
2024-05-17 15:04:11 -0700

    created the category page

--- src/views/admin/Orders.jsx
+++ src/views/admin/Orders.jsx
@@ -14,7 +14,7 @@ const Orders = () => {
             <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
                 {/* Header */}
                 <div className="flex justify-between items-center">
-                    <select className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]">
+                    <select onChange={(e) => setParPage(parseInt(e.target.value))} className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]">
                         <option value="5">5</option>
                         <option value="10">10</option>
                         <option value="20">20</option>
